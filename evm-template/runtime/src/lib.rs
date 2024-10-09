806ef63527a902162e1140e5a30d88cd5c2b2a10
Ozgun Ozerk <ozgun@subspace.network>
2024-10-09 15:58:55 +0300

    added pallets to runtime

--- evm-template/runtime/src/lib.rs
+++ evm-template/runtime/src/lib.rs
@@ -212,6 +212,8 @@ construct_runtime!(
         PolkadotXcm: pallet_xcm = 31,
         CumulusXcm: cumulus_pallet_xcm = 32,
         MessageQueue: pallet_message_queue = 33,
+        XTokens: orml_xtokens::{Pallet, Call, Storage, Event<T>} = 34,
+        XcmWeightTrader: pallet_xcm_weight_trader::{Pallet, Call, Storage, Event<T>} = 35,
 
         // EVM
         Ethereum: pallet_ethereum = 40,
